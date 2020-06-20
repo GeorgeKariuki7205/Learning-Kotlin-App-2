3e98de5ae43224ba7ef4daec966534a716b6b7f6
GeorgeKariuki7205 <ngugigeorge697@gmail.com>
2020-06-20 13:22:50 +0300

    Added Navigation Drawer

--- app/src/main/java/com/example/learningkotlinapp2/MainActivity.kt
+++ app/src/main/java/com/example/learningkotlinapp2/MainActivity.kt
@@ -5,33 +5,37 @@ import com.example.learningkotlinapp2.databinding.ActivityMainBinding
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         // This section of the application is used to add the contentView by the use of the DataBinding Utility.
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-
         toolbar = binding.toolbar
         setSupportActionBar(toolbar)
 
         // This section of the code is used to add the UP-NAVIGATION in application.
-
+        drawerLayout = binding.drawer
         val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
     }
 
     // This section of the code is used to add the UP-NAVIGATION in application.
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.myNavHostFragment)
-        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
+
     }
 }
-
